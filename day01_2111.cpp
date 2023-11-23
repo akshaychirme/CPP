b2d96d7ec5ca772136602805a3535dc4956c734a
akshaychirme <akshaychirme29@gmail.com>
2023-11-23 14:48:29 +0530

    day1

---
+++ day01_2111.cpp
@@ -0,0 +1,74 @@
+// ============    Uncomment the code (Ctrl+/) which you want to RUN ============
+
+/*
+**********************	OBJECT ORIENTED PROGRAMMING WITH C++	***********************
+
+♠ C++ = C + OOPS CONCEPT
+♠ 1980
+♠ Bjarne Stroustrup
+♠ Bell Lab
+♠ Befor c++ Smalltalk, Ada, Simula 67, Turbo Pascal
+♠ First Name - C with classes.
+♠ There are 60 Keywords.
+
+♠ printf = cout << Insertion Operator
+♠ scanf = cin >> Extraction Operator
+*/
+
+//Write a simple output program. 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// 	int a,b,c;
+// 	cout << "Enter value of a and b = ";
+// 	cin >> a >> b;
+// 	c = a + b;
+// 	cout << "\n c = " <<c;
+// }
+
+/*
+♠ INTRODUCTION TO OOP'S CONSEPT
+• 1) Procedure Oriented Programming:-
+
+Drawbacks of procedure Orientede Programming
+1) Complexity increases when no. of lines increases (50000<) because data are global.
+2) Not suitable for real life application.
+3) Reusibility is limited.
+4) It dose not provide data security.
+
+• 2) Object Oriented programming:-
+		C++, JAVA, PHP, Python, DotNET
+	Object --> Data
+		   --> Method (Function)
+• Both the drawbacks are removed in object oriented programming.
+• Data are not global because it is accosiated with an object.
+• So length of program will not increase.
+• because of in real life we first deside the object and then apply the procedure therefore it is suitable for real life applicaion.
+• data is seperate but function can be same.
+
+♠ Concept of Object oriented Programming :-
+	1) Object
+	2) Class
+	3) Abstraction
+	4) Encapsulation
+	5) Polymorphism
+	6) Inheritance
+	7) Dynamic Binding
+
+1) Object :-
+	- object is a variable in oop.
+	- Object is a user define variable.
+	- It is a main entity of oop.
+		Fruit		 	  Mango
+	  Data type			 object
+	User Define D.T		Variable
+	- Behaviour of an object in oop is same as behaviour of an integer variable in oop.
+	- Objects are class type variable.
+
+2) Class :-
+	- Class is a user define data type.
+	- Class is a collection of data and function.
+*/
+
+
---
+++ day02_2211.cpp
@@ -0,0 +1,29 @@
+// ============    Uncomment the code (Ctrl+/) which you want to RUN ============
+
+/*
+3) Data Abtraction :-
+	- Representing only the essential details in the program.
+	- Without knowing the background details we work with the object.
+	- For Ex :- while buying mobile we look for details like ram, processor, camera, battery, price,etc. We dont 
+		look for where the ram is placed inside the mobile body. We dont care which item is placed where by company. So here only essential item are important to us.
+
+4) Data Encapsulation :-
+	- Binding of data and function is called as Encapsulation.
+	- We treat object as a single entity.
+	- If we want to access data we have to access function.
+	- For Ex - To use Ram, processor, Camera,etc they need to bind together and form a single entity (Mobile).
+	
+5) Inheritance :-
+	- Inheritance is a process in which one object acquires all the properties and behaviors of its parent object 
+		automatically. In such way, you can reuse, extend or modify the attributes and behaviors which are defined in other class.
+	- Parent 	- key pad phone --> calling + multimedia
+	  child  	- java phone    --> calling + multimedia + camera + touchscreen
+	  sub child - smartphone 	--> calling + multimedia + camera + touchscreen + applications install
+
+6) Polymorphism :- (bahurupiya)
+	- Same object taking multiple forms is called as polymorphism.
+	- For Ex - While capturing photo from mobile then, mobile is acting as a camera device.
+			   While playing songs on mobile then, mobile is acting as a multimedia player.
+
+
+*/
